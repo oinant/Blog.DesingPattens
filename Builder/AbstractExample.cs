c4e46782942199b86423ce73e28d322dbfe88145
oinant <antoine.sauvinet+github@gmail.com>
2015-08-31 15:03:12 +0200

    Renamesolution, add first files for Builder article

---
+++ Builder/AbstractExample.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace Builder
+{
+    public class MyClass
+    {
+        private readonly string _aStringProperty;
+        private readonly bool _aGivenFlag;
+        private readonly bool _anotherFlag;
+
+        public MyClass(string aStringProperty, bool aGivenFlag, bool anotherFlag)
+        {
+            _aStringProperty = aStringProperty;
+            _aGivenFlag = aGivenFlag;
+            _anotherFlag = anotherFlag;
+        }
+
+        public object DoSomeBehavior()
+        {
+            dynamic @object = null;
+            @object.actionAt = DateTime.UtcNow;
+            @object.theName = _aStringProperty;
+
+            if (_aGivenFlag)
+                @object.theName = "the property : " + @object.theName;
+
+            if (_anotherFlag)
+                @object.theName = @object.theName.ToLower();
+
+            return @object;
+        }
+    }
+
+    public class MyClassBuilder
+    {
+        private string _aNameToSet;
+        private bool _prependPropertyDescriptor = false;
+        private bool _lowerizeOutput = false;
+
+        public MyClassBuilder SetTheName(string aNameToSet)
+        {
+            _aNameToSet = aNameToSet;
+            return this;
+        }
+        
+        public MyClassBuilder PrependPropertyDescriptor()
+        {
+            _prependPropertyDescriptor = true;
+            return this;
+        }
+
+        public MyClassBuilder LowerizeOutput()
+        {
+            _lowerizeOutput = true;
+            return this;
+        }
+
+        public MyClass Build()
+        {
+            return new MyClass(_aNameToSet, _prependPropertyDescriptor, _lowerizeOutput);
+        }
+    }
+
+    public class MyClassClient
+    {
+        public void ExecuteTheDefaultUseCase()
+        {
+            var myClass = new MyClassBuilder()
+                .SetTheName("the first use case is simple")
+                .Build();
+
+            var theResult = myClass.DoSomeBehavior();
+        }
+
+        public void ExecuteAnotherUseCase()
+        {
+            var myClass = new MyClassBuilder()
+                .SetTheName("the first another one")
+                .PrependPropertyDescriptor()
+                .LowerizeOutput()
+                .Build();
+
+            var theResult = myClass.DoSomeBehavior();
+        }
+    }
+    
+}
